1604297ba23ef236ae4eab9aa92a933c45fa9aaa
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-03-05 17:23:01 +0500

    Update phone.py

--- phone.py
+++ phone.py
@@ -1,47 +1,56 @@
-from tkinter import *
-from tkinter import scrolledtext
-from functools import partial
-from tkinter import Tk, W, E
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 12:35:46 2021
 
+@author: polevodin-di
+"""
 
-    
-window = Tk()
-window.title("Наберите номер телефона")
-window.geometry('250x250')
-#txt = scrolledtext.ScrolledText(window,width=20,height=1)
-txt = Entry(window,width=20)  
+import tkinter as tk
+from tkinter import messagebox 
+import random
+import time
 
-def clicked(n):
-#    txt.delete(1.0, END)
-    txt.insert(INSERT, n)
+window = tk.Tk()
+window.title("Игра: Кости")
 
+"""Переменные для игры"""
+user_score = 0
+admin_score = 0
 
-#Кнопка
-btn1 = Button(window, text="1", command=partial(clicked, 1))
-btn2 = Button(window, text="2", command=partial(clicked, 2))
-btn3 = Button(window, text="3", command=partial(clicked, 3))
-btn4 = Button(window, text="4", command=partial(clicked, 4))
-btn5 = Button(window, text="5", command=partial(clicked, 5))
-btn6 = Button(window, text="6", command=partial(clicked, 6))
-btn7 = Button(window, text="7", command=partial(clicked, 7))
-btn8 = Button(window, text="8", command=partial(clicked, 8))
-btn9 = Button(window, text="9", command=partial(clicked, 9))
-btn10 = Button(window, text="", command=partial(clicked, ''))
-btn11 = Button(window, text="0", command=partial(clicked, 0))
-btn12 = Button(window, text="#", command=partial(clicked, '#'))
+""" Функция броска кубика"""
+def click_roll():
+    global user_score
+    global admin_score
+    roll_user = random.randint(1,6)
+    roll_admin = random.randint(1,6)
+    time.sleep(1)
+    if roll_user > roll_admin:
+        user_score += 1
+    if roll_user < roll_admin:
+        admin_score += 1
+    if roll_user == roll_admin:
+        messagebox.showinfo('Так тоже бывает...', 'У нас ничья в этой партии!')
+    txt.delete(0, END)
+    txt.insert(0, 'У тебя на кубике выпало - {}, мой результат {}'.format(roll_user, roll_admin)) 
+    label2.configure(text='Общий счет игры: Пользователь - {}, Админ - {}'.format(user_score, admin_score))
 
-txt.grid(column= 0, row=0)
-btn1.grid(column=1, row=2)
-btn2.grid(column=2, row=2)
-btn3.grid(column=3, row=2)
-btn4.grid(column=1, row=3)
-btn5.grid(column=2, row=3)
-btn6.grid(column=3, row=3)
-btn7.grid(column=1, row=4)
-btn8.grid(column=2, row=4)
-btn9.grid(column=3, row=4)
-btn10.grid(column=1, row=5)
-btn11.grid(column=2, row=5)
-btn12.grid(column=3, row=5)
+""" настройка виджетов """
+txt = Entry(window,width=60)  
+label = tk.Label(text='Бросаем кубик по очереди. Выигрывает тот, у кого выпадет большее число от 1 до 6')
+label2 = tk.Label()
+button = tk.Button(
+    text='Бросить',
+    width=10,
+    height=2,
+    font=('Calibri', 11, 'bold'),
+    bg='white',
+    fg='black',
+    command=partial(click_roll))
+
+""" создаем геометрию """
+label.grid()
+button.grid()
+txt.grid()
+label2.grid()
 
 window.mainloop()
